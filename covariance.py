Fix bug in sample covariance

covariance.py
@@ -6,5 +6,5 @@ with open('./gaussmix.csv') as f:
         data.append([float(x) for x in line.strip().split(',')])
 
 data = np.asarray(data)
-means = [np.mean(x) for x in data.T]
-variance = sum([(x-means)**2 for x in data])/200
+means = np.array([np.mean(x) for x in data.T])
+variance = sum(np.array([np.outer(x-means, x-means) for x in data]))/200